d54762ed1be8f01bbe13fb580fff90a173f73df9
subi <subi.the.dream.walker@gmail.com>
2012-11-06 00:03:04 +0800

    cleanups

--- hooks/distcc.bash
+++ hooks/distcc.bash
@@ -6,7 +6,7 @@ source ${PALUDIS_ECHO_FUNCTIONS_DIR:-${PALUDIS_EBUILD_DIR}}/echo_functions.bash
 if [[ -n ${DISTCC_DIR} ]]; then
     case "${HOOK}" in
 	ebuild_compile_pre|ebuild_install_pre)
-	    einfo_unhooked "Allowing net connection by distcc.."
+	    einfo_unhooked "Allowing net connection for distcc..."
 	    esandbox allow_net --connect "inet:0.0.0.0/0@3632"
 	    esandbox allow_net "unix:/tmp/distcc-pump.*/socket"
 	    ;;
