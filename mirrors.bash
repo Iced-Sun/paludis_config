0dd17b37860323f7617074235a8c73b597b3c679
subi <subi.the.dream.walker@gmail.com>
2013-01-30 15:27:13 +0800

    Update mirrors.bash.

--- mirrors.bash
+++ mirrors.bash
@@ -29,33 +29,26 @@ add_site()
 
 MIRRORS=()
 
-## IPv6 mirrors
-BJTU6=( http://mirror6.bjtu.edu.cn
-    apache cpan cran 'ctan CTAN' debian fedora freebsd 'gentoo gentoo/distfiles' gimp gnome gnu gnu-alpha gnupg kde 'kernel kernel/pub' openbsd sourceforge )
-TSU6=( http://mirrors.6.tuna.tsinghua.edu.cn
-    'ctan CTAN' debian fedora freebsd 'gentoo gentoo/distfiles' gnu 'kernel kernel/pub' opensuse sourceforge )
-USTC6=( http://mirrors.ustc.edu.cn
-    'cpan CPAN' 'cran CRAN' 'ctan CTAN' debian fedora freebsd 'gentoo gentoo/distfiles' gnome gnu kde opensuse )
-FUNET=( ftp://ftp.funet.fi/pub/mirrors 'gnu-alpha alpha.gnu.org' )
-HEANET=( ftp://ftp.heanet.ie/mirrors gnupg 'openbsd OpenBSD' )
-HEANET1=( http://heanet.dl.sourceforge.net sourceforge )
+## IPv4/v6 mirrors
+BJTU=( http://debian.bjtu.edu.cn
+    apache cpan cran 'ctan CTAN' debian freebsd 'gentoo gentoo/distfiles' gimp gnome gnu kde 'kernel kernel/pub' 'libreoffice tdf/libreoffice' )
+TSU=( http://mirrors.tuna.tsinghua.edu.cn
+    apache 'ctan CTAN' 'cran CRAN' 'ctan CTAN' debian freebsd 'gentoo gentoo/distfiles' gnu 'kernel kernel/pub' opensuse )
+USTC=( http://mirrors.ustc.edu.cn
+    'cpan CPAN' 'cran CRAN' 'ctan CTAN' debian freebsd 'gentoo gentoo/distfiles' gnome gnu kde 'kernel linux-kernel' opensuse )
+HEANET=( http://heanet.dl.sourceforge.net sourceforge )
 
-## IPv4 mirrors
-BJTU=( http://mirror.bjtu.edu.cn
-    apache cpan cran 'ctan CTAN' debian fedora freebsd 'gentoo gentoo/distfiles' gimp gnome gnu gnu-alpha gnupg kde 'kernel kernel/pub' openbsd sourceforge )
+## IPv4 only mirrors
 NETEASE=( http://mirrors.163.com
-    cpan debian fedora 'freebsd FreeBSD' 'gentoo gentoo/distfiles' 'opensuse openSUSE')
+    cpan debian 'freebsd FreeBSD' 'gentoo gentoo/distfiles' 'opensuse openSUSE')
 
-if ( ifconfig | grep 'inet6 2001:' ) >/dev/null 2>&1; then
-    add_site "${BJTU6[@]}"
-    add_site "${TSU6[@]}"
-    add_site "${USTC6[@]}"
-    add_site "${FUNET[@]}"
-    add_site "${HEANET[@]}"
-    add_site "${HEANET1[@]}"
-fi
-add_site "${NETEASE[@]}"
 add_site "${BJTU[@]}"
+add_site "${TSU[@]}"
+add_site "${USTC[@]}"
+add_site "${HEANET[@]}"
+#if ( ifconfig | grep 'inet6 2001:' ) >/dev/null 2>&1; then
+#fi
+add_site "${NETEASE[@]}"
 
 ## echo mirrors
 for m in "${MIRRORS[@]}"; do
