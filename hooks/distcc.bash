929543c8aadc19d0d893dc880deb89b8b1478424
Bing Sun <subi.the.dream.walker@gmail.com>
2013-11-12 14:54:33 +0800

    sydbox: failed in testing 'esandbox allow_net'.

--- hooks/distcc.bash
+++ hooks/distcc.bash
@@ -6,10 +6,14 @@ source ${PALUDIS_ECHO_FUNCTIONS_DIR:-${PALUDIS_EBUILD_DIR}}/echo_functions.bash
 if [[ -n ${DISTCC_DIR} ]]; then
     case "${HOOK}" in
 	ebuild_configure_pre | ebuild_compile_pre | ebuild_install_pre )
-	    einfo_unhooked "Allowing net connection for distcc..."
-	    edo esandbox allow_net --connect "inet:0.0.0.0/0@3632"
+	    # broken sandboxing
+	    true
+#	    einfo_unhooked "Allowing net connection for distcc..."
+#	    edo test -e '/dev/sydbox/whitelist/network/connect+inet:10.2.112.0/24@3632'
+#	    edo esandbox disable_net
+#	    edo esandbox allow_net --connect "inet:0.0.0.0/0@3632"
 #	    edo esandbox allow_net "unix:/tmp/distcc-pump.*/socket"
-	    einfo_unhooked "Allowing net connection for distcc...Done"
+#	    einfo_unhooked "Allowing net connection for distcc...Done"
 	    ;;
 	*)
 	    ;;
