1e4856cb76b357082d2b11fff4cd7ac620e03770
Bing Sun <bsun@pku.edu.cn>
2014-02-20 18:09:57 +0800

    Dynamic general.conf.

---
+++ general.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo root = /
+
+if [[ `hostname` == "laptop-x61" ]]; then
+    echo world = ${root}/var/db/paludis/repositories/installed/world
+else
+    echo world = ${root}/var/local/db/paludis/repositories/installed/world
+fi
