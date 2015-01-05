016e556c8b58026982ef9768c27b596330fa43ae
Bing Sun <bsun@pku.edu.cn>
2015-01-05 23:26:58 +0800

    made exherbo-binary work

--- repositories/exherbo-binary.bash
+++ repositories/exherbo-binary.bash
@@ -2,7 +2,7 @@
 
 cat <<EOF
 format = e
-location = ${root}/var/db/paludis/repositories/subi-bin
+location = ${root}/var/db/paludis/repositories/exherbo-binary
 sync = git+ssh://git@github.com/sunbing81/subi-bin.git
 sync_options = --no-reset
 importance = 100
@@ -11,8 +11,8 @@ EOF
 if [[ `hostname` == "fs-3.bir.pku.edu.cn" ]]; then
     cat <<EOF
 binary_destination = true
-binary_distdir = /srv/subi-bin
+binary_distdir = /media/btrfs_raid6_MD1000_4_WD1003FBYX/linux/exherbo-binary
 binary_keywords_filter = amd64 ~amd64
-binary_uri_prefix = mirror://subi-bin/
+binary_uri_prefix = mirror://exherbo-binary/
 EOF
 fi
