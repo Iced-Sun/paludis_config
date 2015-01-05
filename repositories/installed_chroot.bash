696d59cb403e103c1d63a5662dd94aed879cf4e5
Bing Sun <subi.the.dream.walker@gmail.com>
2015-01-05 17:37:04 +0800

    make cave silent

--- repositories/installed_chroot.bash
+++ repositories/installed_chroot.bash
@@ -2,13 +2,13 @@
 
 echo format = exndbam
 
-if [[ `hostname` == "laptop-x61" ]]; then
-#    echo location = ${root}/var/db/paludis/repositories/installed
-    echo "error"
-else
+if [[ `hostname` == "fs-3.bir.pku.edu.cn" ]]; then
     chroot=/media/btrfs_raid6_MD1000_4_WD1003FBYX/linux/os/base/exherbo
     echo root = ${chroot}
     echo name = installed-base
     echo location = ${chroot}/var/db/paludis/repositories/installed
+else
+    echo location = /var/empty
+    echo name = should-not-exist
 fi
 
