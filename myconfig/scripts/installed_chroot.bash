f785a6f83914fd3fb9c89233821cda62fc7fe57a
Bing Sun <bsun@pku.edu.cn>
2015-01-06 12:42:42 +0800

    cleanup

--- myconfig/scripts/installed_chroot.bash
+++ myconfig/scripts/installed_chroot.bash
@@ -2,14 +2,9 @@
 
 echo format = exndbam
 
-if [[ `hostname` == "fs-3.bir.pku.edu.cn" ]]; then
-    chroot=/media/btrfs_raid6_MD1000_4_WD1003FBYX/linux/os/base/exherbo
-    echo root = ${chroot}
-    echo name = installed-base
-    echo location = /var/db/paludis/repositories/installed/chroot/minimal
-else
-    echo root = should-not-exist
-    echo name = should-not-exist
-    echo location = /var/empty
-fi
+chroot=/media/btrfs_raid6_MD1000_4_WD1003FBYX/linux/os/base/exherbo
+echo root = ${chroot}
+echo name = installed-base
+echo location = /var/db/paludis/repositories/installed/chroot/minimal
+
 
