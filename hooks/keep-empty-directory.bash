baf460a8f71ee475b78f0df7d720005324dab613
Bing Sun <bsun@pku.edu.cn>
2015-09-28 15:11:07 +0800

    fix  prefix

--- hooks/keep-empty-directory.bash
+++ hooks/keep-empty-directory.bash
@@ -2,11 +2,13 @@
 
 case "${HOOK}" in
     ebuild_install_post )
-	for DIR in $(find ${IMAGE} -type d -empty); do
+	pushd ${IMAGE} >/dev/null
+	for DIR in $(find . -type d -empty); do
 	    einfo_unhooked "Keep empty directory of ${DIR} to avoid merger failure..."
 
             edo keepdir "${DIR#.}"
 	done
+	popd >/dev/null
 	;;
     *)
         ;;
