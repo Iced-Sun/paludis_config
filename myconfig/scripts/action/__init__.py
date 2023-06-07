21e1ae7d4bd86f15a775dbb81acba46d047bdd21
SUN Bing <subi.the.dream.walker@gmail.com>
2023-06-08 00:20:44 +0800

    feature: support repositories/installed

--- myconfig/scripts/action/__init__.py
+++ myconfig/scripts/action/__init__.py
@@ -1,6 +1,29 @@
 class General_handler:
     @property
-    def configuration(self):
+    def configuration(self) -> str:
         return 'world = ${root}/var/db/paludis/repositories/installed/world'
+
+    pass
+
+class Repository_handler:
+    _repository_name: str
+
+    def __init__(self, repository_name: str):
+        self._repository_name = repository_name
+
+    @property
+    def configuration(self) -> str:
+        match self._repository_name:
+            case 'installed':
+                return '''format = exndbam
+location = ${root}/var/db/paludis/repositories/installed
+name = installed
+split_debug_location = /usr/${CHOST}/lib/debug
+tool_prefix = ${CHOST}-
+'''
+            case _:
+                return 'ttt'
+                #raise new Exception('do not support the repo')
+        pass
 
     pass
--- myconfig/scripts/invoker.py
+++ myconfig/scripts/invoker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from action import General_handler
+from action import General_handler, Repository_handler
 
 import sys
 from pathlib import Path
@@ -7,13 +7,15 @@ from pathlib import Path
 # the first argument is always the script itself
 if len(sys.argv) > 1:
     # <action>.bash or <action>
-    action = Path(sys.argv[1]).name
-    if (action.endswith('.bash')):
-        action = action.split('.')[0]
-        pass
+    script_path = Path(sys.argv[1])
+    suffix = script_path.suffix
+    action = str(script_path).split('/')[0]
+    sub_action = '' if script_path.stem == action else script_path.stem
     pass
 
 match action:
     case 'general':
         print(General_handler().configuration)
         pass
+    case 'repositories':
+        print(Repository_handler(sub_action).configuration)
