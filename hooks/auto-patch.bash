7ec21311215be11db8969c223c169e2c0a5931aa
SUN Bing <subi.the.dream.walker@gmail.com>
2023-06-15 23:48:26 +0800

    chore: style cleanup

--- hooks/auto-patch.bash
+++ hooks/auto-patch.bash
@@ -3,15 +3,14 @@
 PATCH_DIR="/etc/paludis/myconfig/patches"
 
 case "${HOOK}" in
-    ebuild_prepare_post )
-	if [[ -d ${PATCH_DIR}/${CATEGORY}/${PN} ]]; then
-	    for PATCH in `find ${PATCH_DIR}/${CATEGORY}/${PN} -type f -iname '*.patch'`; do
-		einfo "Applying patch ${PATCH}..."
-		expatch "${PATCH}"
-	    done
-	fi
-	;;
-    *)
-	;;
-esac   
-
+	ebuild_prepare_post )
+		if [[ -d ${PATCH_DIR}/${CATEGORY}/${PN} ]]; then
+			for PATCH in `find ${PATCH_DIR}/${CATEGORY}/${PN} -type f -iname '*.patch'`; do
+				einfo "Applying patch ${PATCH}..."
+				expatch "${PATCH}"
+			done
+		fi
+		;;
+	*)
+		;;
+esac
--- hooks/keep-empty-directory.bash
+++ hooks/keep-empty-directory.bash
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 case "${HOOK}" in
-    ebuild_install_post )
-	pushd ${IMAGE} >/dev/null
-	for DIR in $(find . -type d -empty); do
-	    einfo_unhooked "Keep empty directory of ${DIR} to avoid merger failure..."
+	ebuild_install_post )
+		pushd ${IMAGE} >/dev/null
+		for DIR in $(find . -type d -empty); do
+			einfo_unhooked "Keep empty directory of ${DIR} to avoid merger failure..."
 
-            edo keepdir "${DIR#.}"
-	done
-	popd >/dev/null
-	;;
-    *)
-        ;;
-esac   
+			edo keepdir "${DIR#.}"
+		done
+		popd >/dev/null
+		;;
+	*)
+		;;
+esac
