8574bbe82f6b7f705e887863645edc74a03579af
SUN Bing <subi.the.dream.walker@gmail.com>
2025-07-22 10:23:05 +0800

    feat: support unavailable repositories

--- myconfig/scripts/action/repositories_handler.py
+++ myconfig/scripts/action/repositories_handler.py
@@ -10,14 +10,22 @@ class Repository_handler(Host_mixin, Set_files_mixin, Action_handler):
     @property
     def configuration(self) -> str:
         if self._sub_action == 'installed':
-            arch_target_pattern_match = Action_handler.arch_target_pattern.match(self.host)
             format = 'exndbam'
+            arch_target_pattern_match = Action_handler.arch_target_pattern.match(self.host)
+            pass
+        elif Action_handler.arch_target_pattern.match(self._sub_action):
+            format = 'exndbam'
+            arch_target_pattern_match = Action_handler.arch_target_pattern.match(self._sub_action)
+            pass
+        elif self._sub_action.startswith('unavailable'):
+            format = 'unavailable'
             pass
 
-        elif Action_handler.arch_target_pattern.match(self._sub_action):
-            arch_target_pattern_match = Action_handler.arch_target_pattern.match(self._sub_action)
-            format = 'exndbam'
-            pass
+        configurations = [
+            f'format = {format}',
+            f'name = {self._sub_action}'
+        ]
+        location = self._sub_action
 
         if format == 'exndbam':
             target_triple = {
@@ -29,24 +37,33 @@ class Repository_handler(Host_mixin, Set_files_mixin, Action_handler):
                 'env': arch_target_pattern_match.group(5)
             }
 
-            if self._sub_action == 'installed':
-                location = 'installed'
-                pass
-            else:
+            if self._sub_action != 'installed':
                 location = f'cross-installed/{target_triple["target"]}'
+                configurations.append(f'cross_compile_host = {target_triple["target"]}')
                 pass
 
-            configurations = [
-                'format = exndbam',
+            configurations += [
                 f'location = $root/var/db/paludis/repositories/{location}',
                 f'name = {self._sub_action}',
                 f'split_debug_location = /usr/{target_triple["target"]}/lib/debug',
                 f'tool_prefix = {target_triple["target"]}-'
             ]
 
-            if self._sub_action != 'installed':
-                configurations.append(f'cross_compile_host = {target_triple["target"]}')
+            pass
+
+        elif format == 'unavailable':
+            if self._sub_action == 'unavailable':
+                tar = 'exherbo_repositories.tar.bz2'
                 pass
+            elif self._sub_action == 'unavailable-unofficial':
+                tar = 'exherbo_unofficial_repositories.tar.bz2'
+                pass
+
+            configurations += [
+                f'location = $root/var/db/paludis/repositories/{location}',
+                f'sync = tar+https://unavailable.exherbolinux.org/{tar}',
+                'importance = -100'
+            ]
             pass
 
         return '\n'.join(configurations)
